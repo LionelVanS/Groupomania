a514401adeffbb58487abf784edb0535167c399d
LionelVanS <lionelvs@outlook.fr>
2022-09-01 21:27:45 +0200

    Connexion BDD, Helmet, Entetes, Fichiers statiques, UserRoutes

---
+++ Back/app.js
@@ -0,0 +1,43 @@
+// Appel des dependances
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const helmet = require('helmet')
+
+// Appel des différentes routes
+const userRoutes = require('./routes/user')
+
+// Création de l'application Express
+const app = express()
+
+// Récupération des réponses de requêtes en JSON
+app.use(express.json())
+
+// Ajout du middleware HELMET
+app.use(helmet())
+
+// Connexion à la DATABASE MongoDB Atlas 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/test?retryWrites=true&w=majority`,
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Modification des en-têtes pour autoriser le fonctionnement de l'application
+// sur plusieurs port (port 3000 pour le backend et port 4200 pour le frontend)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
+    next()
+})
+
+// Enregistrement des images dans le dossier 'images'
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+// Routes
+app.use('/', userRoutes)
+
+module.exports = app
