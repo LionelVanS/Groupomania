bca205aef4924b143a98b649fbeee8223a489cdf
LionelVanS <lionelvs@outlook.fr>
2022-09-07 15:47:41 +0200

    Ajout de l'app avec les routes home,  signup, login et erreur 404

---
+++ Front/groupomania/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js'
+import Signup from './pages/Signup.js'
+import Login from './pages/Login.js'
+import Error404 from './pages/Error404.js';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path = '/' element = {<Home />} />
+        <Route path = '/signup' element = {<Signup />} />
+        <Route path = '/login' element = {<Login />} />
+        <Route path = '/*' element = {<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ Front/groupomania/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <Navigation />
+        </div>
+    );
+};
+
+export default Login;
