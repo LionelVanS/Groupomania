43af8829d2c41eda9f1b6d96d730f69f5fc35a9c
LionelVanS <lionelvs@outlook.fr>
2022-09-08 17:25:40 +0200

    Ajout d'un message d'erreur

--- Front/groupomania/src/pages/Error404.js
+++ Front/groupomania/src/pages/Error404.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Header from '../components/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBan } from '@fortawesome/free-solid-svg-icons'
 
 const Error404 = () => {
     return (
-        <div>
-            <h1>La page n'existe pas</h1>
-        </div>
+        <>
+            <Header />
+            <main id="error">
+                <FontAwesomeIcon icon= { faBan } size="6x" color="#4E5166" />
+                <h2>La page recherchée n'existe pas</h2>
+            </main>
+        </>
     );
 };
 
-export default Error404;<h1>La page n'existe pas</h1>
+export default Error404 ;
