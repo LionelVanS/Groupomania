642de362be6aa9d9415e1363ae4bd4947e3c3fa4
LionelVanS <lionelvs@outlook.fr>
2022-09-08 17:32:00 +0200

    Affichage du formulaire de connexion OK

---
+++ Front/groupomania/src/components/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Login = () => (
+    <main className="login">
+        <div className="login__form">
+            <label htmlFor="user-email">Adresse mail:</label>
+            <input id="user-email" classname="user-input" type="text" required />
+            <label htmlFor="user-password">Mot de Passe:</label>
+            <input id="user-password" classname="user-input" type="text" required />
+            <button>Valider</button>
+        </div>
+        <div className="dot"></div>
+        
+        <NavLink to="/signup">
+            <li>
+                S'inscrire
+            </li>
+        </NavLink>
+    </main>
+)
+
+export default Login; 
