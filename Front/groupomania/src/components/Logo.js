781cea7da14e4fb92d68dcd6f597613b7d27100e
LionelVanS <lionelvs@outlook.fr>
2022-09-08 17:31:27 +0200

    Logo OK

---
+++ Front/groupomania/src/components/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <img src="./images/icon-left-font-monochrome-white.svg" alt="Logo Groupomania" />
+    );
+};
+
+export default Logo;
