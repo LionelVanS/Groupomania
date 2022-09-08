f8719bd119397970fc54ac837519eab014002eba
LionelVanS <lionelvs@outlook.fr>
2022-09-08 17:32:47 +0200

    Affichage du formulaire d'inscription OK

---
+++ Front/groupomania/src/components/SignupForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SignupForm = () => {
+    return (
+        <>
+            <main classname="signup">
+                <h1 className="signup__title">Veuillez remplir le formulaire suivant pour s'inscrire</h1>
+                <div className="signup__form">
+                    <label htmlFor="user-email">Adresse mail:</label>
+                    <input id="user-email" classname="user-input" type="text" required />
+                    <label htmlFor="user-password">Mot de Passe:</label>
+                    <input id="user-password" classname="user-input" type="text" required />
+                    <button>Valider</button>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default SignupForm;
