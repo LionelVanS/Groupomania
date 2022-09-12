29f6930d4afac6344d2d12fbba0ac585b2de15f1
LionelVanS <lionelvs@outlook.fr>
2022-09-12 16:12:20 +0200

    Modification de la gestion des erreurs

--- Back/controllers/user.js
+++ Back/controllers/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
-// Appel des fonctions
+// Appel des fonctions de vérification de format mot de passe et email
 const { checkEmailFormat, checkPassword } = require('../middleware/checkNewUserData')
 
 // Appel du modèle utilisateur
@@ -11,40 +11,44 @@ const User = require('../models/user')
 
 // Signup
 async function signup(req, res){
-    if (!User){
-        const isPasswordIsValid = checkPassword(req)
-        const isEmailIsValid = checkEmailFormat(req)
 
-    if(isEmailIsValid && isPasswordIsValid){
-        const hash = await bcrypt.hash(req.body.password, 10)
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        })
-        await user.save()
-        res.status(200).json({ message: 'Utilisateur créé' })
-    }} else {
-        res.status(400).json({ message: 'Impossible d\'enregistrer l\'utilisateur' })
-    }
-}
-
+        const  isPasswordIsValid  = checkPassword(req)
+        const  isEmailIsValid  = checkEmailFormat(req)
+            if(isEmailIsValid && isPasswordIsValid){
+                const hash = await bcrypt.hash(req.body.password, 10)
+                try{
+                    const user = new User({
+                        email: req.body.email,
+                        password: hash
+                    })
+                    await user.save()
+                    res.status(200).json(({ message: 'Utilisateur créé' }))
+                } catch(e){
+                    res.status(400).json({ message: 'L\'email doit être unique' })
+                }
+            } else{
+                res.status(400).json({ message: 'Les champs de saisie ne sont pas correctement renseignés' })
+            }
+        }
+        
 // Login
 async function login(req, res){
     try{
         const user = await User.findOne({ email: req.body.email })
-        const isValid = await bcrypt.compare(req.body.password, user.password)
+
+        const userIsValid = await bcrypt.compare(req.body.password, user.password)
         
-        if(isValid){
+        if(userIsValid){
             res.status(200).json({
                 userId: user._id,
                 token: jwt.sign({ userId: user._id }, process.env.JWT_SECRETKEY, { expiresIn: '24h' }),
                 message : 'Utilisateur connecté'
             });
         } else {
-            res.status(401).json({ message: 'Utilisateur non connecté' })
+            res.status(401).json({ message: 'Mot de passe erroné' })
         }
-    } catch {
-        res.status(401).json({ message: 'Utilisateur non connecté'})
+    } catch (err){
+        res.status(400).json({ message: 'Utilisateur inconnu' })
     }
 }
     
