383b3d3c3c807778550bdc1f8bc35fdaac841751
LionelVanS <lionelvs@outlook.fr>
2022-09-12 13:18:23 +0200

    Création de la page création de post

---
+++ Front/groupomania/src/pages/createPost/createPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const createPost = () => {
+    
+    return (
+        <>
+            <textarea name="textarea1" id="" cols="30" rows="10"></textarea>
+        </>
+    );
+};
+
+export default createPost;
