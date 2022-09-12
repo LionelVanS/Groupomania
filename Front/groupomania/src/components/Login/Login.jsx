f6dbe0a5cfb991cac7e7f1a60d78840bdbb083ed
LionelVanS <lionelvs@outlook.fr>
2022-09-12 13:18:01 +0200

    Requete POST pour le log OK

--- Front/groupomania/src/components/Login/Login.jsx
+++ Front/groupomania/src/components/Login/Login.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import axios from 'axios'
 
-const Login = () => (
-    <aside className="login">
-        <div className="login__form">
-            <label htmlFor="user-email">Adresse mail:</label>
-            <input id="user-email" classname="user-input" type="text" required />
-            <label htmlFor="user-password">Mot de Passe:</label>
-            <input id="user-password" classname="user-input" type="text" required />
-            <button>Valider</button>
-        </div>
-    </aside>
-)
+const Login = () => {
+    function Log(){
+        const email = document.getElementById("email-login-input")
+        const password = document.getElementById("password-login-input")
+
+        axios.post("http://localhost:3001/login", { 
+            email: email.value,
+            password: password.value
+         })
+            .then( res => {
+                // const token = res.data.token
+
+            })
+    }
+
+    return(
+        <aside className="login">
+            <form className="login__form">
+                <label>
+                    <input id="email-login-input" className="user-input" type="text" placeholder="Adresse mail" required />
+                </label>
+
+                <label>
+                    <input id="password-login-input" className="user-input" type="text" placeholder="Mot De Passe" required />
+                </label>
+
+                <button type="button" onClick={() => Log()}>Valider</button>
+            </form>
+        </aside>
+    )
+}
 
 export default Login; 
