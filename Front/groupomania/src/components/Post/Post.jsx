17d5ad37bca35cbeccf4aadae00ac18fb92c4ecd
LionelVanS <lionelvs@outlook.fr>
2022-09-16 13:02:25 +0200

    Ajout des icones edit/delete et like

--- Front/groupomania/src/components/Post/Post.jsx
+++ Front/groupomania/src/components/Post/Post.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
 
 const Post = () => {
     return (
         <>
             <article className="post">
                 <div className="post__user">
-                    <img src="./images/avatar.png" alt="Photo de profil" />
-                    <p className='post__content__user-name'>Lionel Van Schellebeck</p>
+                    <img src="./images/avatar.png" alt="profil" />
+                    <p className='post__user__user-surname'>Lionel</p>
+                    <p className='post__user__user-name'>Van S</p>
                 </div>
                 
-                <div className="post__content">
-                    <p className="post__content__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Doloribus hic vitae enim nihil tempore tenetur quidem officia minima ea. Eius 
-                    repellat, ipsam, aperiam in nihil odit laudantium architecto neque nam quisquam magnam
-                    aspernatur eaque libero reprehenderit hic quae labore quidem. Sapiente, quasi dolores
-                    sequi suscipit placeat excepturi expedita officiis ex qui saepe deleniti debitis sed 
-                    est, veniam quisquam, nemo tenetur odio vero eligendi dolore quae nesciunt fugit. 
-                    Libero dolores illum ipsa eum eveniet modi officia vel a corporis cumque eos, optio 
-                    id porro veritatis quis possimus numquam dolore, ullam laboriosam. Voluptatum itaque, 
-                    atque qui magnam iure aut dolorum fugit laborum!</p>
-                    <p className="post__content__date">le 10 Septembre 2022, à 16h40</p>
+                <div className="post__container">
+                        <h3> Mon super Article </h3>
+                    <div className="post__container__content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus hic vitae enim nihil tempore tenetur quidem officia minima ea. Eius 
+                        repellat, ipsam, aperiam in nihil odit laudantium architecto neque nam quisquam magnam
+                        aspernatur eaque libero reprehenderit hic quae labore quidem. Sapiente, quasi dolores
+                        sequi suscipit placeat excepturi expedita officiis ex qui saepe deleniti debitis sed 
+                        est, veniam quisquam, nemo tenetur odio vero eligendi dolore quae nesciunt fugit. 
+                        Libero dolores illum ipsa eum eveniet modi officia vel a corporis cumque eos, optio 
+                        id porro veritatis quis possimus numquam dolore, ullam laboriosam. Voluptatum itaque, 
+                        atque qui magnam iure aut dolorum fugit laborum!</p>
+                        <img src="https://picsum.photos/200/300" alt="post"></img>
+                    </div>
+    
+                    <div className="post__container__like">
+                        <FontAwesomeIcon icon= {  faThumbsUp } size="2x"  className="like icon" />
+                        <FontAwesomeIcon icon= {  faThumbsDown } size="2x" className="dislike icon" />
+                    </div>
+
+                    <div className="post__container__data">
+                        <p>le 10 Septembre 2022, à 16h40</p>
+                        <div className="post__container__data__edit">
+                            <FontAwesomeIcon icon= {  faPenToSquare } size="2x"  className="icon edit" />
+                            <FontAwesomeIcon icon= {  faTrashCan } size="2x"  className="icon delete" />
+                        </div>
+                    </div>
                 </div>
             </article>
         </>
