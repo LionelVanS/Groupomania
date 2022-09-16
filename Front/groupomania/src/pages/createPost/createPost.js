7e9509efa509503a8fc93a1b30e7b60bf0316ba6
LionelVanS <lionelvs@outlook.fr>
2022-09-16 13:03:11 +0200

    Formulaire d'ajout de post

--- Front/groupomania/src/pages/createPost/createPost.js
+++ Front/groupomania/src/pages/createPost/createPost.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
-const createPost = () => {
+const CreatePost = () => {
     
     return (
         <>
-            <textarea name="textarea1" id="" cols="30" rows="10"></textarea>
+        <main id="main-create-post">
+            <h1>Créez un post</h1>
+            <form className="add-post-form">
+                <label>
+                    <input type="text" className="add-post-form__title" placeholder="Titre" required />
+                </label>
+
+                <input type="file" id="input" className="up-pic" required/>
+
+                <label>
+                    <textarea type="text"  placeholder="Tapez votre message ici" required />
+                </label>
+
+                <button type="button" className="btn">Poster</button>
+            </form>
+        </main>
         </>
     );
 };
 
-export default createPost;
+export default CreatePost;
