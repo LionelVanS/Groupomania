5fdf3f8d545625e517afd27df218c854a5904e7c
LionelVanS <lionelvs@outlook.fr>
2022-09-22 15:36:20 +0200

    Refacto

--- Back/models/post.js
+++ Back/models/post.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose')
 const postSchema = mongoose.Schema({
   userId: { type: String, required: true },
   text: { type: String, required: true },
-  imageUrl: { type: String },
-  likes: { type: Number, default: 0, required: true },
-  dislikes: { type: Number, default: 0, required: true },
-  usersLiked: { type: Array, default: [], required: true },
-  usersDisliked: { type: Array, default: [], required: true }
+  picture: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: { type: Array, default: [] },
+  usersDisliked: { type: Array, default: [] },
 })
 
 module.exports = mongoose.model('Post', postSchema)
