fcfb138966e1f68dbb71f4663e187e0bdca9f172
LionelVanS <lionelvs@outlook.fr>
2022-09-22 15:37:09 +0200

    Refacto prettier

--- Back/app.js
+++ Back/app.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose')
 const path = require('path')
 const helmet = require('helmet')
 
-
 // Appel des différentes routes
 const userRoutes = require('./routes/user')
 const userPost = require('./routes/post')
@@ -19,28 +18,34 @@ app.use(express.json())
 // Ajout du middleware HELMET
 app.use(helmet())
 
-// Connexion à la DATABASE MongoDB Atlas 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/test?retryWrites=true&w=majority`,
-{ useNewUrlParser: true,
-  useUnifiedTopology: true })
+// Connexion à la DATABASE MongoDB Atlas
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/test?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 // Modification des en-têtes pour autoriser le fonctionnement de l'application
-// sur plusieurs port (port 3000 pour le backend et port 4200 pour le frontend)
+// sur plusieurs port (port 3001 pour le backend et port 3000 pour le frontend)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
-    next()
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site')
+  next()
 })
-
-// Enregistrement des images dans le dossier 'images'
-app.use('/images', express.static(path.join(__dirname, 'images')))
 
 // Routes
 app.use('/', userRoutes)
 app.use('/', userPost)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 module.exports = app
