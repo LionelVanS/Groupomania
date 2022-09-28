d8c132f44682813d63b83d7d89771cdf749e0628
LionelVanS <lionelvs@outlook.fr>
2022-09-28 13:21:28 +0200

    Ajout des routes modifications et obtention d'un utilisateur

--- Back/routes/user.js
+++ Back/routes/user.js
@@ -1,6 +1,8 @@
 // Appel de la fonction Router d'Express
 const express = require('express')
 const router = express.Router()
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer')
 
 // Appel du contrôleur utilisateur
 const userCtrl = require('../controllers/user')
@@ -8,6 +10,8 @@ const userCtrl = require('../controllers/user')
 // Création des routes Utilisateurs
 router.post('/signup', userCtrl.signup)
 router.post('/login', userCtrl.login)
-router.put('/updateUser', userCtrl.updateUser)
+router.put('/updateUser', auth, multer, userCtrl.updateUser)
+router.get('/getOneUser', auth, userCtrl.getOneUser)
+router.get('/getPostUser/:id', auth, userCtrl.getPostUser)
 
 module.exports = router
