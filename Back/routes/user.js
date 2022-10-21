5dd23392d7162756fb5939420b9ae26a46f57c0b
LionelVanS <lionelvs@outlook.fr>
2022-10-22 01:21:19 +0200

    Ajout du limiteur d'essai de connexion à la route login

--- Back/routes/user.js
+++ Back/routes/user.js
@@ -3,13 +3,14 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
 const multer = require('../middleware/multer')
+const limiter = require('../middleware/rateLimite')
 
 // Appel du contrôleur utilisateur
 const userCtrl = require('../controllers/user')
 
 // Création des routes Utilisateurs
 router.post('/signup', userCtrl.signup)
-router.post('/login', userCtrl.login)
+router.post('/login', limiter, userCtrl.login)
 router.put('/updateUser', auth, multer, userCtrl.updateUser)
 router.get('/getOneUser', auth, userCtrl.getOneUser)
 router.get('/getPostUser/:id', auth, userCtrl.getPostUser)
