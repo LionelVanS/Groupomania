9c30faaa5694d7a8623c58d897220891efb5ffe1
LionelVanS <lionelvs@outlook.fr>
2022-10-22 01:33:12 +0200

    Ajout de composants

--- Front/groupomania/src/components/Header/Header.jsx
+++ Front/groupomania/src/components/Header/Header.jsx
@@ -1,49 +1,60 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 
-// MUI
-import { Button } from '@mui/material'
+// Composants
+import NavBar from '../NavBar/NavBar'
+import UserData from '../UserData/UserData'
+import MobileHeader from './MobileHeader/MobileHeader'
 
-const Header = ({ userIsLog, setUserIsLog }) => {
-   // Déconnexion de l'utilisateur
-   const disconnectUser = () => {
-      sessionStorage.clear()
-      setUserIsLog(false)
-   }
+const Header = ({ userIsLog, setUserIsLog, user, mobile, tablet }) => {
+   // UseStates
+   const [profilIsComplet, setProfilIsComplet] = useState() // Est ce que le profil utilisateur est complet ?
+
+   // Contrôle de la complétion du profil utilisateur
+   useEffect(() => {
+      if (user !== null) {
+         if ('name' in user) {
+            setProfilIsComplet(true)
+         } else {
+            setProfilIsComplet(false)
+         }
+      } else {
+         return
+      }
+   }, [])
 
    // RENDER
    return (
       <header>
-         <img src="./images/logo.png" alt="Logo Groupomania" />
-
-         {userIsLog ? (
-            <nav>
-               <ul>
-                  <NavLink to="/home">
-                     <Button>Accueil</Button>
-                  </NavLink>
-                  <NavLink to="/createPost">
-                     <Button>Nouveau Post</Button>
-                  </NavLink>
-                  <NavLink to="/updateuser">
-                     <Button>Modifier votre Profil</Button>
-                  </NavLink>
-               </ul>
-            </nav>
+         {mobile || tablet ? (
+            <MobileHeader
+               user={user}
+               userIsLog={userIsLog}
+               setUserIsLog={setUserIsLog}
+               profilIsComplet={profilIsComplet}
+               setProfilIsComplet={setProfilIsComplet}
+               mobile={mobile}
+               tablet={tablet}
+            />
          ) : (
             ' '
          )}
-         <div className="disconnect">
-            <NavLink
-               to="/"
-               className={
-                  userIsLog ? 'disconnect__btn' : 'disconnect__btn display-none'
-               }
-               onClick={() => disconnectUser()}
-            >
-               Déconnexion
-            </NavLink>
-         </div>
+
+         {!mobile && !tablet ? (
+            <div className="header__content">
+               <div className="header__content__img">
+                  <img src="./images/logo.png" alt="Logo Groupomania" />
+               </div>
+               {userIsLog && profilIsComplet ? <UserData user={user} /> : ''}
+            </div>
+         ) : (
+            ' '
+         )}
+
+         {userIsLog && !mobile && !tablet ? (
+            <NavBar setUserIsLog={setUserIsLog} />
+         ) : (
+            ' '
+         )}
       </header>
    )
 }
